8f1dd0e67cc008970285b377126163aa10f69372
Gaurav Saxena <sgaurav8819@gmail.com>
2020-04-02 14:04:36 +0530

    Initial Commit

---
+++ src/main/java/com/mav/business/uam/UamApplication.java
@@ -0,0 +1,13 @@
+package com.mav.business.uam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mav/business/uam/UamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mav.business.uam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
