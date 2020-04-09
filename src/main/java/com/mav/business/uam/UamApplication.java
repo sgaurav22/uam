bdb36df02585c150b3748f31ac924c23a12a9798
Gaurav Saxena <gaurav.saxena@nttdsdwsrnd.com>
2020-04-09 20:52:57 +0530

    Added banner for application.
    Signed-off-by: Gaurav Saxena <gaurav.saxena@nttdsdwsrnd.com>

--- src/main/java/com/mav/business/uam/UamApplication.java
+++ src/main/java/com/mav/business/uam/UamApplication.java
@@ -1,5 +1,6 @@
 package com.mav.business.uam;
 
+import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UamApplication.class, args);
+		SpringApplication app = new SpringApplication(UamApplication.class);
+		app.setLogStartupInfo(false);
+		app.run(args);
 	}
 
 }
